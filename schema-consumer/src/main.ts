28d6d2864a8bfc50fd151abf6ab4a4dc70850394
sasaki-s-sci <sasaki.s.sci@gmail.com>
2026-01-05 22:46:22 +0900

    test

---
+++ schema-consumer/src/main.ts
@@ -0,0 +1,7 @@
+import { type SchemaSample, testFunction } from "@sasaki-s-sci/schema-provider";
+
+const schemaSample: SchemaSample = {
+    id: "",
+    value: 0,
+};
+const main = () => testFunction();
